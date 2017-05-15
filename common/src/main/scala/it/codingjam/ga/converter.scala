950af999721492391f1f8e3437ec7964bbef7baa
Giampaolo <giampaolo.trapasso@gmail.com>
2017-05-15 20:48:12 +0200

    8 bit

--- common/src/main/scala/it/codingjam/ga/converter.scala
+++ common/src/main/scala/it/codingjam/ga/converter.scala
@@ -10,10 +10,10 @@ package object converter {
 
     val x = parse(gene.binaryString.substring(0, 8))
     val y = parse(gene.binaryString.substring(8, 16))
-    val radius = (parse(gene.binaryString.substring(16, 20)) + 1).toInt
-    val red = parse(gene.binaryString.substring(20, 26)) * 4
-    val green = parse(gene.binaryString.substring(26, 32)) * 4
-    val blue = parse(gene.binaryString.substring(32, 38)) * 4
+    val radius = (parse(gene.binaryString.substring(16, 24)))
+    val red = parse(gene.binaryString.substring(24, 32))
+    val green = parse(gene.binaryString.substring(32, 40))
+    val blue = parse(gene.binaryString.substring(40, 48))
 
     (x, y, radius, red, green, blue)
   }
